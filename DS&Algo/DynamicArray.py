dd2ad47537672d9011c52517bd64f15c0ca1ed99
das <pkd1964@gmail.com>
2017-12-23 14:09:05 +0530

    Importing data in python

--- DS&Algo/DynamicArray.py
+++ DS&Algo/DynamicArray.py
@@ -38,3 +38,23 @@ class DynamicArray:
     def _make_array(self, c):
         """Return a new array with capacity c"""
         return (c * ctypes.py_object)()
+
+    def insert(self,k,value):
+        """Inserting an element in index k, shifting elements rightwards"""
+        if self._n == self._capacity:
+            self._resize(2*self._capacity)
+        for j in range(self._n, k, -1):
+            self._A[j]=self._A[j-1]
+        self._A[k] = value
+        self._n += 1
+
+    def remove(self, value):
+        """Remove the element in the list if it occurs or raise error"""
+        for k in range(self._n):
+            if self._A[k] == value:
+                for j in range(k, self._n-1):
+                    self._A[j] = self._A[j+1]
+                self._A[self._n-1] = None
+                self._n -=1
+                return
+        raise ValueError('Value not found')
