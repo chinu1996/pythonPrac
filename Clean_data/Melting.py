e6ebb7b29d7b7c3944eeec9378005bd168da9c0f
das <pkd1964@gmail.com>
2017-12-25 23:52:53 +0530

    Cleaning data in python

--- Clean_data/Melting.py
+++ Clean_data/Melting.py
@@ -2,7 +2,8 @@
 print(airquality.head())
 
 # Melt airquality: airquality_melt
-airquality_melt = pd.melt(frame=df, id_vars=['Month', 'Day'])
+airquality_melt = pd.melt(frame=df, id_vars=['Month', 'Day'], var_name='measurement', value_name='reading')
 
 # Print the head of airquality_melt
 print(airquality_melt.head())
+
---
+++ Clean_data/Reset_index.py
@@ -0,0 +1,28 @@
+# Print the index of airquality_pivot
+print(airquality_pivot.index)
+# There's a very simple method you can use to get back the original DataFrame from the pivoted DataFrame:
+# .reset_index()
+
+# Reset the index of airquality_pivot: airquality_pivot
+airquality_pivot = airquality_pivot.reset_index()
+
+# Print the new index of airquality_pivot
+print(airquality_pivot.index)
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+
+
+
+# Pivot airquality_dup: airquality_pivot
+airquality_pivot = airquality_dup.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
+
+# Reset the index of airquality_pivot
+airquality_pivot = airquality_pivot.reset_index()
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+# Print the head of airquality
+print(airquality.head())
